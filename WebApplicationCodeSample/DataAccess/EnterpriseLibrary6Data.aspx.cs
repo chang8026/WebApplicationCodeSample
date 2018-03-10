5d1129890c9f4e5b2c41fd4070d9000be0b8c81b
chang8026 <chang8026@gmail.com>
2018-03-10 23:20:52 +0900

    초기 커밋.

---
+++ WebApplicationCodeSample/DataAccess/EnterpriseLibrary6Data.aspx.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationCodeSample.DataAccess
+{
+	public partial class EnterpriseLibrary6Data : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			DatabaseProviderFactory factory = new DatabaseProviderFactory();
+
+			Database LocalDB = factory.Create("LocalDB");
+
+			DataSet ds = LocalDB.ExecuteDataSet(CommandType.Text, "select * From TestTable");
+
+			GridView1.DataSource = ds;
+			GridView1.DataBind();
+		}
+	}
+}
---
+++ WebApplicationCodeSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationCodeSample.Global" Language="C#" %>
